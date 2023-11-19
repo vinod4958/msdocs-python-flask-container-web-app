86d342e5ee780ea7838a3439160f75abf0e8d591
RekhuGopal <vrchinnarathod@gmail.com>
2023-11-19 20:27:30 +0530

    updated changes

--- iac/app_service.tf
+++ iac/app_service.tf
@@ -38,6 +38,6 @@ resource "azurerm_app_service" "backend" {
 resource "azurerm_role_assignment" "acr_pull" {
   principal_id                     = azurerm_app_service.backend.identity[0].principal_id
   role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.registry[0].id
+  scope                            = azurerm_container_registry.registry.id
   skip_service_principal_aad_check = true
 }
