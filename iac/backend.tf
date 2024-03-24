e799e114ddf0b3347aa035ce12467cbe3872b7ea
vinod4958 <68851068+vinod4958@users.noreply.github.com>
2024-03-24 08:56:02 -0400

    Update backend.tf

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,16 +1,16 @@
 terraform {
-  required_version = ">=1.0"
+  // required_version = ">=1.0"
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.80.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
-  }
+  // required_providers {
+  //   azurerm = {
+  //     source  = "hashicorp/azurerm"
+  //     version = "~>3.80.0"
+  //   }
+  //   random = {
+  //     source  = "hashicorp/random"
+  //     version = "~>3.0"
+  //   }
+  // }
   // backend "remote" {
 	// 	hostname = "app.terraform.io"
 	// 	organization = "VincTech"
